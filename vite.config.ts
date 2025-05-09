376b8a09c7f480c20180b39e4dd68f889c3d3a41
eliaschampi <eliaschampi@proton.com>
2025-05-09 13:55:28 -0500

    fix: Update version in package.json and switch to adapter-node in svelte.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	ssr: {
-		// Indica a Vite que no externalice/procese este módulo nativo
-		noExternal: ['@u4/opencv4nodejs']
-	},
-	// Opcional: A veces también ayuda excluirlo de optimizeDeps
-	optimizeDeps: {
-		exclude: ['@u4/opencv4nodejs']
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
